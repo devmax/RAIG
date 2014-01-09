ce429142a03ffc346189c918d76f453f0f8d4ead
Devendra Goyal <Dev@Devendras-MacBook-Pro.local>
2014-01-09 01:00:49 -0500

    Particle Filter 0.2

--- python/particle.py
+++ python/particle.py
@@ -1,19 +1,19 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
 class ParticleFilter:
 
-    particles = None
-    w = None
+    def __init__(self, Np, dim):
 
-    def __init__(self, N, dim):
-
-        self.N = N
+        self.Np = Np
         self.dim = dim
 
-        particles = np.empty([self.dim, self.N], dtype=float)
-        w = np.empty(self.N, dtype=float)
+        self.particles = np.empty([self.dim+1, self.Np], dtype=float)
+        self.w = np.ones(self.Np)
+
+        self.process = []
 
     def setProcessModel(self, omega, bias):
 
@@ -26,11 +26,13 @@ class ParticleFilter:
 
     def populateInitial(self, omega, bias):
 
-        self.particles[0] = np.random.normal(omega[0], omega[1], self.N)
+        # omega should be a list/tuple of two values - mean and variance
+        self.particles[0] = np.random.normal(omega[0], omega[1], self.Np)
 
-        for i in xrange(self.dim-1):
+        # bias should be a list of lists/tuples
+        for i in xrange(self.dim):
             self.particles[(i+1)] = np.random.normal(bias[i][0],
-                                                     bias[i][1], self.N)
+                                                     bias[i][1], self.Np)
 
     def draw(self, weights, choices, count):
 
@@ -47,46 +49,66 @@ class ParticleFilter:
                 beta -= weights[idx]
                 idx = (idx + 1) % N
 
-            samples[i] = choices[idx]
+            samples[:, i] = choices[:, idx]
 
         return samples
 
     def predict(self):
 
         self.particles[0] += np.random.normal(self.process[0][0],
-                                              self.process[0][1], self.N)
+                                              self.process[0][1], self.Np)
 
-        for i in xrange(self.dim-1):
-            self.particles[i+1] += np.random.normal(self.bias[i+1][0],
-                                                    self.bias[i+1][1], self.N)
+        for i in xrange(self.dim):
+            self.particles[i+1] += np.random.normal(self.process[i+1][0],
+                                                    self.process[i+1][1],
+                                                    self.Np)
 
     def Gaussian(self, x, mu, sigma):
-        return math.exp((-0.5)*pow(((x-mu)/sigma),
-                                   2))/(math.sqrt(2)*sigma*np.pi)
+        return (1/(math.sqrt(2)*sigma))*math.exp((-0.5)*pow(((x-mu)/sigma), 2))
 
     def measure(self, Z):
 
-        assert Z.dim == self.dim-1
+        assert Z.shape[0] == self.dim
 
-        for i in xrange(self.N):
-            self.w[i] = self.Gaussian(Z[0]-self.particles[0] -
-                                      self.particles[i+1], self.meas[i][0],
-                                      self.meas[i][1])
+        for j in xrange(self.Np):
+            for i in xrange(self.dim):
+                x = Z[i]-self.particles[0, j]-self.particles[i+1, j]
+                p = self.Gaussian(x, self.meas[i][0], self.meas[i][1])
+                self.w[j] = self.w[j]*p
 
     def getEstimate(self):
 
-        return self.particles[np.argmax(self.w)]
+        return self.particles[:, np.argmax(self.w)]
 
     def resample(self):
 
-        self.particles = np.copy(self.draw(self.w, self.particles, self.N))
+        self.particles = np.copy(self.draw(self.w, self.particles, self.Np))
+        self.w = np.ones(self.Np)*1./self.Np*1.
 
 
 def runFilter(obs, omega):
 
-    pf = ParticleFilter(obs.shape[1], obs.shape[0])
+    pf = ParticleFilter(50, obs.shape[0])
 
+    pf.populateInitial([omega[0], 0.05], [[obs[i, 0]-omega[0], 0.01]
+                                          for i in xrange(pf.dim)])
     pf.setProcessModel((0.0, 0.0085), [(0.0, 0.00015)]*pf.dim)
-    pf.setMeasNoise((0.0, 0.00001)*pf.dim)
+    pf.setMeasNoise([(0.0, 0.001)]*pf.dim)
 
-    
+    estimate = np.empty([obs.shape[1], obs.shape[0]+1])
+    Z = obs.T
+
+    N = obs.shape[1]
+
+    for i in xrange(N):
+        pf.predict()
+        pf.measure(Z[i])
+
+        estimate[i] = pf.getEstimate()
+
+        pf.resample()
+
+    plt.plot(omega[:N], 'g')
+    plt.plot(estimate[:, 0], 'r')
+
+    plt.show()
